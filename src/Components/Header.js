5d9700f99490088bc1e210153c9d70e07db08a90
Bai Jaitrong <bjaitrong@comcast.net>
2023-03-24 22:13:47 -0600

    Add Header page and css styling.

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./../header.css";
+
+export default function Header() {
+  return (
+    <>
+      <h1>Welcome To Rise & Grind's Inventory System</h1>
+      <footer>
+        <p>Image Attribution:</p>
+        <a href="https://huggingface.co/spaces/dalle-mini/dalle-mini">DALL·E mini by craiyon.com</a>
+      </footer>
+    </>
+  );
+}
